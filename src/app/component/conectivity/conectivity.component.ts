7adbd827cb2389a866eceab71427d2529fdd40c3
Ahsan Khan <experts.web2@gmail.com>
2024-10-15 16:23:32 +0500

    Spinner Version match & connectivity component code optimized.

--- src/app/component/conectivity/conectivity.component.ts
+++ src/app/component/conectivity/conectivity.component.ts
@@ -1,5 +1,4 @@
 import { DatePipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -10,73 +9,64 @@ import { ConectivityService } from 'src/app/services/conectivity.service';
   selector: 'app-conectivity',
   templateUrl: './conectivity.component.html',
   styleUrls: ['./conectivity.component.scss'],
-  providers: [DatePipe]
-
+  providers: [DatePipe],
 })
 export class ConectivityComponent implements OnInit {
   panelOpenState = false;
-  currentDate!: string;
-  connectedUser!: IConnectedUser | null;
-  constructor(private route: ActivatedRoute,
-     private router: Router,
-     private datePipe: DatePipe,
-     private conectivityService: ConectivityService,
-     private spinner: NgxSpinnerService) 
-     {
-    let user = localStorage.getItem('user')
-    if(user){
+  connectedUser: IConnectedUser | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private datePipe: DatePipe,
+    private conectivityService: ConectivityService,
+    private spinner: NgxSpinnerService
+  ) {
+    const user = localStorage.getItem('user');
+    if (user) {
       this.connectedUser = JSON.parse(user);
     }
   }
+
   ngOnInit(): void {
-    this.route.queryParams.subscribe((params:any) => {
-      const code = params['code'];
-      const state = params['state'];
+    this.route.queryParams.subscribe(({ code, state }) => {
       const storedState = localStorage.getItem('latestCSRFToken');
- 
-      if (state !== storedState) {
-        return;
-      }
- 
+      if (state !== storedState) return;
+
       localStorage.removeItem('latestCSRFToken');
-      this.getCallback(code,state);
+      this.getCallback(code, state);
     });
   }
 
-  getDate(createAt:string): string {
-    const now = new Date(createAt);
-    return this.datePipe.transform(now, 'yyyy-MM-dd hh:mm a')!;
+  getDate(createAt: string): string {
+    return this.datePipe.transform(new Date(createAt), 'yyyy-MM-dd hh:mm a') || '';
   }
 
-  connect() {
+  connect(): void {
     this.conectivityService.loginWithGitHub();
   }
 
-  getCallback(code:any,state:any){
+  private getCallback(code: string, state: string): void {
     this.spinner.show();
     this.conectivityService.getCallBack({ code, state }).subscribe({
       next: (x) => {
-        this.spinner.hide();
-        localStorage.setItem("user",JSON.stringify(x))
+        localStorage.setItem('user', JSON.stringify(x));
         this.router.navigate(['/']);
-      },
-      error: (err) => {
         this.spinner.hide();
-      }
-    })
+      },
+      error: () => this.spinner.hide(),
+    });
   }
 
-  disconectUser(userToken:any){
+  disconectUser(userToken: string): void {
     this.spinner.show();
     this.conectivityService.disconectUser(userToken).subscribe({
-      next: (x) => {
-        this.spinner.hide();
-        localStorage.removeItem('user')
+      next: () => {
+        localStorage.removeItem('user');
         this.connectedUser = null;
-      },
-      error: (err) => {
         this.spinner.hide();
-      }
-    })
+      },
+      error: () => this.spinner.hide(),
+    });
   }
 }
